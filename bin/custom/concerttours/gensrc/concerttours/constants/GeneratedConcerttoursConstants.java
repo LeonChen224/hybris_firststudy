ff7e96d6410da995e379a897f0eccdae9295ba8d
陈蕾 <Leon.Chen1@connext.com.cn>
2018-03-12 10:43:24 +0800

    Add your Extension

---
+++ bin/custom/concerttours/gensrc/concerttours/constants/GeneratedConcerttoursConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 2018-3-12 9:58:12                           ---
+ * ----------------------------------------------------------------
+ */
+package concerttours.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedConcerttoursConstants
+{
+	public static final String EXTENSIONNAME = "concerttours";
+	
+	protected GeneratedConcerttoursConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
